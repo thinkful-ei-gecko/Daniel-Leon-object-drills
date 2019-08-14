75050c17a8e790783de1fffbf3f535f810c3f694
Daniel <herrnichols@gmail.com>
2019-08-14 16:52:16 -0400

    bonus

--- drills.js
+++ drills.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*// Drill #1
 
 let loaf = {
@@ -138,3 +139,27 @@ let found = characters.find((element) => element.nickname === 'Aragorn');
 found.describe();
 let hobbitPeople = characters.filter((element) => element.race === 'Hobbit');
 let aboveFive = characters.filter((element) => element.attack > 5);
+
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let prop in query) {
+      if (query[prop] === arr[i][prop]) {
+        return null;
+      }
+    }
+    return arr[i];
+  }
+}
+
+console.log(findOne(HEROES, { id: 2 }));
